39dd4a880459a2f5b896ccb6461c4bfb3ef0b896
Aleksander Aleksiev <aleksiev.t@gmail.com>
2020-11-15 14:24:22 +1100

    Typo fix

--- OffPiste/coverage.sh
+++ OffPiste/coverage.sh
@@ -37,7 +37,7 @@ fi
 echo "Rebuilding library with coverage flags!"
 echo "(Note: rebuilding library with coverage flags uses GCC)"
 cd build
-cmake -D COVERAGE=ON -D CMAKE_FOO_COMPILER=GNU .
+cmake -D COVERAGE=ON -D CMAKE_C_COMPILER=GNU .
 make install
 cd ..
 
