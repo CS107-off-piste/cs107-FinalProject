0d8df41562ff49b8422a28795500e1ad4cfe3101
Aleksander Aleksiev <aleksiev.t@gmail.com>
2020-11-17 00:20:58 +1100

    Run updated source files through formatter

--- OffPiste/include/OffPiste.hpp
+++ OffPiste/include/OffPiste.hpp
@@ -6,8 +6,9 @@
 /* system header files */
 #ifndef DOXYGEN_IGNORE
 
-#  include <stdio.h>
-#  include <cmath>
+#include <stdio.h>
+
+#include <cmath>
 
 #endif
 
@@ -16,55 +17,53 @@
 /* =============== */
 template <class T>
 class AutoDiff {
-  private:
-    T v;    /**< value */
-    T dv;   /**< derivative value */
+ private:
+  T v;  /**< value */
+  T dv; /**< derivative value */
 
-  public:
+ public:
+  /* constructors */
+  AutoDiff(){};
+  AutoDiff(T val, T dval) : v(val), dv(dval){};
 
-    /* constructors */
-    AutoDiff(){};
-    AutoDiff(T val,T dval) : v(val),dv(dval) {};
+  /* getters */
+  T val() const { return v; };
+  T dval() const { return dv; };
 
-    /* getters */
-    T val() const {return v;};
-    T dval() const {return dv;};
+  /* setters */
+  void setval(T val) { v = val; }
+  void setdval(T dval) { dv = dval; }
 
-    /* setters */
-    void setval(T val){v = val;}
-    void setdval(T dval){dv = dval;}
+  void print_hello() const;
 
-    void print_hello() const;
+  /* ==================== */
+  /* overloaded operators */
+  /* ==================== */
+  const AutoDiff<T> operator+(const AutoDiff<T> &other) const {
+    return AutoDiff<T>(v + other.val(), dv + other.dval());
+  }
 
-    /* ==================== */
-    /* overloaded operators */
-    /* ==================== */
-    const AutoDiff<T> operator+(const AutoDiff<T> &other) const{
-        return AutoDiff<T>(v + other.val(), dv + other.dval());
-    }
+  AutoDiff<T> &operator+=(const AutoDiff<T> &other) {
+    v += other.val();
+    dv += other.dval();
+    return *this;
+  }
 
-    AutoDiff<T>& operator+=(const AutoDiff<T> &other){
-        v += other.val();
-        dv += other.dval();
-        return *this;
-    }
+  const AutoDiff<T> operator*(const AutoDiff<T> &other) const {
+    return AutoDiff<T>(v * other.val(), v * other.dval() + dv * other.val());
+  }
 
-    const AutoDiff<T> operator*(const AutoDiff<T> &other) const {
-        return AutoDiff<T>(v*other.val(), v*other.dval() + dv*other.val());
-    }
+  AutoDiff<T> &operator*=(const AutoDiff<T> &other) {
+    dv = v * other.dval() + dv * other.val();
+    v *= other.val();
+    return *this;
+  }
 
-    AutoDiff<T> &operator*=(const AutoDiff<T> &other) {
-        dv = v*other.dval() + dv*other.val();
-        v *=other.val();
-        return *this;
-    }
-
-    AutoDiff<T> &operator^(const float alpha) {
-        dv = alpha*std::pow(v, alpha-1) * dv;
-        v = std::pow(v, alpha);
-        return *this;
-    }
-
+  AutoDiff<T> &operator^(const float alpha) {
+    dv = alpha * std::pow(v, alpha - 1) * dv;
+    v = std::pow(v, alpha);
+    return *this;
+  }
 };
 
 #endif /* OFFPISTE_H */
