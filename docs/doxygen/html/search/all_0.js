c5a641f9d7738406a70408ae606bedb7298fc9bf
Aleksander Aleksiev <aleksiev.t@gmail.com>
2020-11-16 18:40:59 +1100

    Re-generate docs based on current source

--- docs/doxygen/html/search/all_0.js
+++ docs/doxygen/html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['autodiff_0',['AutoDiff',['../class_auto_diff.html',1,'AutoDiff&lt; T &gt;'],['../class_auto_diff.html#a0e0d94efe6eb895cdea19ac5c22e4c4f',1,'AutoDiff::AutoDiff()'],['../class_auto_diff.html#a0e0d94efe6eb895cdea19ac5c22e4c4f',1,'AutoDiff::AutoDiff()']]]
+  ['autodiff_0',['AutoDiff',['../class_auto_diff.html',1,'']]]
 ];
--- docs/doxygen/html/search/searchdata.js
+++ docs/doxygen/html/search/searchdata.js
@@ -2,23 +2,20 @@ var indexSectionsWithContent =
 {
   0: "ao",
   1: "a",
-  2: "a",
-  3: "o"
+  2: "o"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "functions",
-  3: "pages"
+  2: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Functions",
-  3: "Pages"
+  2: "Pages"
 };
 
