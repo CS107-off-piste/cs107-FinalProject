e38a09cdd7f9c42da00a1d1d42a01ec2eb884f11
Aleksander Aleksiev <aleksiev.t@gmail.com>
2020-11-17 22:57:59 +1100

    Add failing test for sin() operation

--- OffPiste/core/tests/src/test_operators.cpp
+++ OffPiste/core/tests/src/test_operators.cpp
@@ -119,3 +119,35 @@ TEST(Operators, pow_double) {
     EXPECT_NEAR(y.val(), 1.0, 1E-6);
     EXPECT_NEAR(y.dval(), 5.7, 1E-6);
 }
+
+TEST(Operators, sin) {
+    using AD = AutoDiff<double>;
+
+    double seed1 = 1;
+    double seed2 = 2.0;
+
+    // x1 has value of 2, and a derivative of 1.0
+    AD x1(2.0, seed1);
+    // x2 has value of 3, and a derivative of 2.0
+    AD x2(3.0, seed2);
+
+    // take sin of each one
+    AD y1 = AD::sin(x1);
+    AD y2 = AD::sin(x2);  
+
+    // for the function y = sin(x), we expect that
+    //  -> y_value = sin(x_value)
+    //  -> y_derivative = cos(x_value) * x_derivative 
+
+    // for our object x1, x_value = 2 and x_derivative = 1
+    // so we expect to see y1 having a value of: sin(2) ≈ 0.9093 
+    EXPECT_NEAR(y1.val(), 0.9093, 1E-1);
+    // and we expect a derivative of: cos(2) * 1 ≈ 0.4161 
+    EXPECT_NEAR(y1.dval(), 0.4161, 1E-1);
+
+    // for our object x2, x_value = 3 and x_derivative = 2
+    // so we expect to see y1 having a value of: sin(3) ≈ 0.1411 
+    EXPECT_NEAR(y2.val(), 0.1411, 1E-1);
+    // // and we expect a derivative of: cos(3) * 2 ≈ -1.97998 
+    EXPECT_NEAR(y2.dval(), -1.97998, 1E-1);
+}
