aa5b41500399fb018ff120d43492c722597a6b4c
Aleksander Aleksiev <aleksiev.t@gmail.com>
2020-11-20 13:24:17 +1100

    Update docs to reflect unary negation operation

--- docs/doxygen/html/search/functions_4.js
+++ docs/doxygen/html/search/functions_4.js
@@ -4,7 +4,7 @@ var searchData=
   ['operator_2a_3d_23',['operator*=',['../class_auto_diff.html#a7956ca408be474f95b20a38bcf369645',1,'AutoDiff']]],
   ['operator_2b_24',['operator+',['../class_auto_diff.html#a0be7f1edd1e4cf7bf66b1458c7c28e9a',1,'AutoDiff']]],
   ['operator_2b_3d_25',['operator+=',['../class_auto_diff.html#a478aefdb22027c7bff37dbde034e15fb',1,'AutoDiff']]],
-  ['operator_2d_26',['operator-',['../class_auto_diff.html#a52127fbdbd87107e4390c5a07d13efb3',1,'AutoDiff']]],
+  ['operator_2d_26',['operator-',['../class_auto_diff.html#aaf5d4466861bc9eaa8a700f1e1fda391',1,'AutoDiff::operator-() const'],['../class_auto_diff.html#a52127fbdbd87107e4390c5a07d13efb3',1,'AutoDiff::operator-(const AutoDiff &amp;node) const']]],
   ['operator_2d_3d_27',['operator-=',['../class_auto_diff.html#af466b8d44fdbbe7a5d7c5ae6f4d91161',1,'AutoDiff']]],
   ['operator_2f_28',['operator/',['../class_auto_diff.html#af6d310307a51b74209e490bcf0bd8864',1,'AutoDiff']]],
   ['operator_2f_3d_29',['operator/=',['../class_auto_diff.html#a5241ac827df9674bc7be5e05dd840dd7',1,'AutoDiff']]],
