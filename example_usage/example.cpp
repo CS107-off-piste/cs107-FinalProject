e5f4b187df89dd6f15d4b0f261de8859e92e547c
Aleksander Aleksiev <aleksiev.t@gmail.com>
2020-12-09 22:12:38 +1100

    Update example_usage directory for new implementation

--- example_usage/example.cpp
+++ example_usage/example.cpp
@@ -1,28 +1,94 @@
 #include <iostream>
-#include "../OffPiste/install/include/OffPiste.hpp"
+#include "../OffPiste/include/Variable.hpp"
+#include "../OffPiste/include/Function.hpp"
+#include "../OffPiste/include/BinaryOperator.hpp"
+#include "../OffPiste/include/UnaryOperator.hpp"
 
-typedef AutoDiff AD;
+using namespace OP;
 
-int main(int argc, char const *argv[])
-{
-    // some test values
-    double value1 = 1.2; 
-    double value2 = 3.4;
-    double seed1 = 1.0;
-    double seed2 = 1.0;
+int main() {
 
-    // construct an AD object 
-    // AD is just a shortcut for AutoDiff<double>
-    AD x(value1,seed1);
-    AD y(value2,seed2);
+    // A function f with 3 inputs a, b, c and 2 outputs u, v.
+    float value1 = 1;
+    float value2 = 2;
+    float value3 = 3;
 
-    // print initial value and seed
-    std::cout << "Initial x, y value: " << x.val() << ", "<< y.val() << "\n"; 
-    std::cout << "Initial x, y deriv: " << x.dval() << ", "<< y.dval() << "\n";
+    float seed1 = 0;
+    float seed2 = 0;
+    float seed3 = 1;
 
-    // perform the operation z = e^(sin(x + y))
-    AD z = AD::exp(AD::sin(x+y));
-    std::cout << "Z value, derivative: " << z.val() << ", "<< z.dval() << "\n"; 
+    Variable a(value1, seed1);
+    Variable b(value2, seed2);
+    Variable c(value3, seed3);
+
+    // Each Expression is a scalar function of vector input
+    Expression u = a - b + 1;
+    Expression v = a + exp(1 + c);
+
+    // Construct the computational graph called f.
+    Function f({a,b,c}, {u, v});
+
+    /** Forward Mode Example **/
+    std::cout<<"Forward Example:"<<std::endl;
+
+    // evaluate the computational graph and get forward derivative for free!
+    Vec vec_f = f.evaluate();
+    std::cout<<"u = "<<u.val<<std::endl;    // get output value by output node
+    std::cout<<"v = "<<v.val<<std::endl;    // get output value by output node
+    std::cout<<"(u, v) = ("<<vec_f[0]<<", "<<vec_f[1]<<")"<<std::endl;      // get output values by vec
+
+    std::cout<<std::endl;
+    std::cout<<"du/dseed = "<<u.dval<<std::endl;
+    std::cout<<"dv/dseed = "<<v.dval<<std::endl;
+
+    std::cout<<std::endl<<"Jacobian Matrix of (u, v) w.r.t (a, b, c): "<<std::endl;
+    Mat jacob_f = f.forward_jacobian();
+    for (auto &i : jacob_f) {
+        for (auto &j : i) {
+            std::cout<<j<<" ";
+        }
+        std::cout<<std::endl;
+    }
+
+
+    /** Reverse Mode Example **/
+    std::cout<<std::endl<<"Backward Example:"<<std::endl;
+
+    // Combine input nodes and output nodes together respectively
+    Input inputs = {a, b, c};
+    Output outputs = {u, v};
+
+    // onstruct the computational graph called g by inputs and outputs
+    Function g(inputs, outputs);
+
+    Vec seeds = {0,0,1};
+    g.set_seed(seeds);
+
+    Vec vec_b = f.evaluate();
+    std::cout<<"u = "<<u.val<<std::endl;    // get output value by output node
+    std::cout<<"v = "<<v.val<<std::endl;    // get output value by output node
+    std::cout<<"(u, v) = ("<<vec_b[0]<<", "<<vec_b[1]<<")"<<std::endl;      // get output values by vec
+
+    std::cout<<std::endl;
+
+    f.zero_grad();      // call f.zero_grad() to set grads of all nodes in the graph f to zero
+    u.backward();
+    std::cout<<"(du/da, du/db, du/dc) = ("<<a.grad<<", "<<b.grad<<", "<<c.grad<<")"<<std::endl;
+
+
+    f.zero_grad();      // call f.zero_grad() to set grads of all nodes in the graph f to zero
+    v.backward();
+    std::cout<<"(dv/da, dv/db, dv/dc) = ("<<a.grad<<", "<<b.grad<<", "<<c.grad<<")"<<std::endl;
+
+    std::cout<<std::endl<<"Jacobian Matrix of (u, v) w.r.t (a, b, c): "<<std::endl;
+    Mat jacob_b = f.backward_jacobian();
+    for (auto &i : jacob_b) {
+        for (auto &j : i) {
+            std::cout<<j<<" ";
+        }
+        std::cout<<std::endl;
+    }
+
 
     return 0;
 }
