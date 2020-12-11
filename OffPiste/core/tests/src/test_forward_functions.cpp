4d0a6718b83ecfb14ace3658d230d3642e1a34af
Krithika Sundararajan <krithika.sundararajan@go-jek.com>
2020-12-11 11:35:58 +0800

    Add tests for Forward mode functions (#38)
    * Add tests for Forward mode binary functions
    
    * Add tests for Unary operators - Part 1
    
    * Add updated ForwardFunctions.hpp
    
    * Add tests for atan, asin, acos
    
    * Add divide by 0 test case, run clang format

---
+++ OffPiste/core/tests/src/test_forward_functions.cpp
@@ -0,0 +1,235 @@
+/* googletest header files */
+#include "gtest/gtest.h"
+
+/* source / header files */
+#include "ForwardFunctions.hpp"
+#include "Node.hpp"
+#include "Variable.hpp"
+#include "test_vars.h"
+
+// add an AD shortcut for brevity
+using namespace OP;
+
+//--------------------------- Binary Functions --------------------------------
+TEST(ForwardFunctions, Add) {
+  // Init
+  Node node;
+  Node child1(2.0, 1.5);
+  Node child2(3.0, 2.0);
+  node._children.push_back(&child1);
+  node._children.push_back(&child2);
+
+  // Add
+  add_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 5.0, 1E-6);
+  EXPECT_NEAR(node.dval, 3.5, 1E-6);
+}
+
+TEST(ForwardFunctions, Subtract) {
+  // Init
+  Node node;
+  Node child1(2.0, 1.5);
+  Node child2(3.0, 2.0);
+  node._children.push_back(&child1);
+  node._children.push_back(&child2);
+
+  // Subtract
+  sub_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, -1.0, 1E-6);
+  EXPECT_NEAR(node.dval, -0.5, 1E-6);
+}
+
+TEST(ForwardFunctions, Multiply) {
+  // Init
+  Node node;
+  Node child1(2.0, 1.5);
+  Node child2(3.0, 2.0);
+  node._children.push_back(&child1);
+  node._children.push_back(&child2);
+
+  // Multiply
+  mul_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 6.0, 1E-6);
+  EXPECT_NEAR(node.dval, 8.5, 1E-6);
+}
+
+TEST(ForwardFunctions, Divide) {
+  // Init
+  Node node1;
+  Node node2;
+  Node child1(1.0, 1.9);
+  Node child2(2.0, 4.4);
+  Node child3(0.0, 0.0);
+
+  node1._children.push_back(&child1);
+  node1._children.push_back(&child2);
+
+  node2._children.push_back(&child1);
+  node2._children.push_back(&child3);
+
+  // Divide
+  div_forward(node1);
+
+  // Validate
+  EXPECT_NEAR(node1.val, 0.5, 1E-4);
+  EXPECT_NEAR(node1.dval, -0.15, 1E-2);
+
+  // Validate Divide by 0
+  try {
+    div_forward(node2);
+  } catch (const char* msg) {
+    EXPECT_EQ(msg, "Divide by zero");
+  }
+  EXPECT_NEAR(node2.val, 0, 1E-8);
+  EXPECT_NEAR(node2.dval, 0, 1E-8);
+}
+
+TEST(ForwardFunctions, Power) {
+  // Init
+  Node node;
+  Node child1(1.0, 1.9);
+  Node child2(3.0, 0.0);
+  node._children.push_back(&child1);
+  node._children.push_back(&child2);
+
+  // Power
+  pow_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 1.0, 1E-6);
+  EXPECT_NEAR(node.dval, 5.7, 1E-2);
+}
+
+//--------------------- Negate, Exponent, Log ---------------------------------
+TEST(ForwardFunctions, Negate) {
+  // Init
+  Node node;
+  Node child1(2.0, 1.5);
+  node._children.push_back(&child1);
+
+  // Negate
+  neg_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, -2.0, 1E-6);
+  EXPECT_NEAR(node.dval, -1.5, 1E-6);
+}
+
+TEST(ForwardFunctions, Exponent) {
+  // Init
+  Node node;
+  Node child1(3.0, 2.0);
+  node._children.push_back(&child1);
+
+  // Exponent
+  exp_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 20.09, 1E-2);
+  EXPECT_NEAR(node.dval, 40.17, 1E-2);
+}
+
+TEST(ForwardFunctions, Logarithm) {
+  // Init
+  Node node;
+  Node child1(2.0, 1.5);
+  node._children.push_back(&child1);
+
+  // Logarithm
+  log_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 0.6931, 1E-4);
+  EXPECT_NEAR(node.dval, 0.75, 1E-4);
+}
+
+//--------------------------- Trigonometric Functions -------------------------
+TEST(ForwardFunctions, Sine) {
+  // Init
+  Node node;
+  Node child1(3.0, 2.0);
+  node._children.push_back(&child1);
+
+  // Sine
+  sin_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 0.1411, 1E-4);
+  EXPECT_NEAR(node.dval, -1.98, 1E-2);
+}
+
+TEST(ForwardFunctions, Cosine) {
+  // Init
+  Node node;
+  Node child1(3.0, 2.0);
+  node._children.push_back(&child1);
+
+  // Cosine
+  cos_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, -0.99, 1E-4);
+  EXPECT_NEAR(node.dval, -0.28, 1E-2);
+}
+
+TEST(ForwardFunctions, Tangent) {
+  // Init
+  Node node;
+  Node child1(3.0, 2.0);
+  node._children.push_back(&child1);
+
+  // Tangent
+  tan_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, -0.1425, 1E-4);
+  EXPECT_NEAR(node.dval, 2.04, 1E-2);
+}
+
+TEST(ForwardFunctions, Asin) {
+  // Init
+  Node node;
+  Node child1(0.9, 0.2);
+  node._children.push_back(&child1);
+
+  // Asin
+  asin_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 1.1198, 1E-4);
+  EXPECT_NEAR(node.dval, 0.46, 1E-2);
+}
+
+TEST(ForwardFunctions, Acos) {
+  // Init
+  Node node;
+  Node child1(0.9, 0.2);
+  node._children.push_back(&child1);
+
+  // Acos
+  acos_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 0.4510, 1E-4);
+  EXPECT_NEAR(node.dval, -0.46, 1E-2);
+}
+
+TEST(ForwardFunctions, Atan) {
+  // Init
+  Node node;
+  Node child1(30.0, 20.0);
+  node._children.push_back(&child1);
+
+  // Atan
+  atan_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 1.5375, 1E-4);
+  EXPECT_NEAR(node.dval, 0.02, 1E-2);
+}
--- OffPiste/include/BackwardFunctions.hpp
+++ OffPiste/include/BackwardFunctions.hpp
@@ -2,79 +2,82 @@
 #define CS107_FINALPROJECT_BACKWARDFUNCTIONS_HPP
 
 #include <cmath>
+
 #include "Node.hpp"
 
 namespace OP {
 
-    void identity_backward(Node &node) {}
+static void identity_backward(Node &node) {}
 
-    /** backward methods for binary operation **/
-    void add_backward(Node &node) {
-
-        node._children[0]->grad += node.grad;
-        node._children[1]->grad += node.grad;
-    }
-
-    void sub_backward(Node &node) {
-        node._children[0]->grad += node.grad;
-        node._children[1]->grad += -node.grad;
-    }
-
-    void mul_backward(Node &node) {
-        node._children[0]->grad += node.grad * node._children[1]->val;
-        node._children[1]->grad += node.grad * node._children[0]->val;
-    }
-
-    void div_backward(Node &node) {
-        node._children[0]->grad += node.grad / node._children[1]->val;
-        node._children[1]->grad += -node.grad * node._children[0]->val
-                / (node._children[1]->val * node._children[1]->val);
-    }
-
-    void pow_backward(Node &node) {
-        node._children[0]->grad += node.grad * node._children[1]->val
-                * std::pow(node._children[0]->val, node._children[1]->val - 1);
-        node._children[1]->grad += node.grad * std::log(node._children[0]->val)
-                * std::exp(node._children[1]->val * std::log(node._children[0]->val));
-    }
-
-
-    /** backward methods for unary operation **/
-    void neg_backward(Node &node) {
-        node._children[0]->grad += -node.grad;
-    }
-
-    void exp_backward(Node &node) {
-        node._children[0]->grad += node.grad * std::exp(node._children[0]->val);
-    }
-
-    void log_backward(Node &node) {
-        node._children[0]->grad += node.grad * 1.f / node._children[0]->val;
-    }
-
-    void sin_backward(Node &node) {
-        node._children[0]->grad += node.grad * std::cos(node._children[0]->val);
-    }
-
-    void cos_backward(Node &node) {
-        node._children[0]->grad += node.grad * -std::sin(node._children[0]->val);
-    }
-
-    void tan_backward(Node &node) {
-        node._children[0]->grad += node.grad * 1 / std::pow(std::cos(node._children[0]->val), 2);
-    }
-
-    void asin_backward(Node &node) {
-        node._children[0]->grad += node.grad * 1.f/(std::sqrt(1 - std::pow(node._children[0]->val, 2)));
-    }
-
-    void acos_backward(Node &node) {
-        node._children[0]->grad += node.grad * -1.f/(std::sqrt(1 - std::pow(node._children[0]->val, 2)));
-    }
-
-    void atan_backward(Node &node) {
-        node._children[0]->grad += node.grad * 1.f/(1 + std::pow(node._children[0]->val, 2));
-    }
-
+/** backward methods for binary operation **/
+static void add_backward(Node &node) {
+  node._children[0]->grad += node.grad;
+  node._children[1]->grad += node.grad;
 }
-#endif //CS107_FINALPROJECT_BACKWARDFUNCTIONS_HPP
+
+static void sub_backward(Node &node) {
+  node._children[0]->grad += node.grad;
+  node._children[1]->grad += -node.grad;
+}
+
+static void mul_backward(Node &node) {
+  node._children[0]->grad += node.grad * node._children[1]->val;
+  node._children[1]->grad += node.grad * node._children[0]->val;
+}
+
+static void div_backward(Node &node) {
+  node._children[0]->grad += node.grad / node._children[1]->val;
+  node._children[1]->grad += -node.grad * node._children[0]->val /
+                             (node._children[1]->val * node._children[1]->val);
+}
+
+static void pow_backward(Node &node) {
+  node._children[0]->grad +=
+      node.grad * node._children[1]->val *
+      std::pow(node._children[0]->val, node._children[1]->val - 1);
+  node._children[1]->grad +=
+      node.grad * std::log(node._children[0]->val) *
+      std::exp(node._children[1]->val * std::log(node._children[0]->val));
+}
+
+/** backward methods for unary operation **/
+static void neg_backward(Node &node) { node._children[0]->grad += -node.grad; }
+
+static void exp_backward(Node &node) {
+  node._children[0]->grad += node.grad * std::exp(node._children[0]->val);
+}
+
+static void log_backward(Node &node) {
+  node._children[0]->grad += node.grad * 1.f / node._children[0]->val;
+}
+
+static void sin_backward(Node &node) {
+  node._children[0]->grad += node.grad * std::cos(node._children[0]->val);
+}
+
+static void cos_backward(Node &node) {
+  node._children[0]->grad += node.grad * -std::sin(node._children[0]->val);
+}
+
+static void tan_backward(Node &node) {
+  node._children[0]->grad +=
+      node.grad * 1 / std::pow(std::cos(node._children[0]->val), 2);
+}
+
+static void asin_backward(Node &node) {
+  node._children[0]->grad +=
+      node.grad * 1.f / (std::sqrt(1 - std::pow(node._children[0]->val, 2)));
+}
+
+static void acos_backward(Node &node) {
+  node._children[0]->grad +=
+      node.grad * -1.f / (std::sqrt(1 - std::pow(node._children[0]->val, 2)));
+}
+
+static void atan_backward(Node &node) {
+  node._children[0]->grad +=
+      node.grad * 1.f / (1 + std::pow(node._children[0]->val, 2));
+}
+
+}  // namespace OP
+#endif  // CS107_FINALPROJECT_BACKWARDFUNCTIONS_HPP
