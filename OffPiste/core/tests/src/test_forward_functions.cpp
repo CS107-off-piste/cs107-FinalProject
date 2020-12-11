5b307088c0e307c2dd31395853348cb825e4a20b
Krithika Sundararajan <krithika.sundararajan@go-jek.com>
2020-12-11 10:47:30 +0800

    Add tests for Unary operators - Part 1

--- OffPiste/core/tests/src/test_forward_functions.cpp
+++ OffPiste/core/tests/src/test_forward_functions.cpp
@@ -10,6 +10,7 @@
 // add an AD shortcut for brevity
 using namespace OP;
 
+//--------------------------- Binary Functions --------------------------------
 TEST(ForwardFunctions, Add) {
   // Init
   Node node;
@@ -61,8 +62,8 @@ TEST(ForwardFunctions, Multiply) {
 TEST(ForwardFunctions, Divide) {
   // Init
   Node node;
-  Node child1(2.0, 1.5);
-  Node child2(3.0, 2.0);
+  Node child1(1.0, 1.9);
+  Node child2(2.0, 4.4);
   node._children.push_back(&child1);
   node._children.push_back(&child2);
 
@@ -70,15 +71,15 @@ TEST(ForwardFunctions, Divide) {
   div_forward(node);
 
   // Validate
-  EXPECT_NEAR(node.val, 0.6667, 1E-4);
-  EXPECT_NEAR(node.dval, 0.06, 1E-2);
+  EXPECT_NEAR(node.val, 0.5, 1E-4);
+  EXPECT_NEAR(node.dval, -0.15, 1E-2);
 }
 
 TEST(ForwardFunctions, Power) {
   // Init
   Node node;
-  Node child1(2.0, 1.5);
-  Node child2(3.0, 2.0);
+  Node child1(1.0, 1.9);
+  Node child2(3.0, 0.0);
   node._children.push_back(&child1);
   node._children.push_back(&child2);
 
@@ -86,6 +87,92 @@ TEST(ForwardFunctions, Power) {
   pow_forward(node);
 
   // Validate
-  EXPECT_NEAR(node.val, 8.0, 1E-6);
-  EXPECT_NEAR(node.dval, 29.09, 1E-2);
+  EXPECT_NEAR(node.val, 1.0, 1E-6);
+  EXPECT_NEAR(node.dval, 5.7, 1E-2);
+}
+
+//--------------------- Negate, Exponent, Log ---------------------------------
+TEST(ForwardFunctions, Negate) {
+  // Init
+  Node node;
+  Node child1(2.0, 1.5);
+  node._children.push_back(&child1);
+
+  // Negate
+  neg_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, -2.0, 1E-6);
+  EXPECT_NEAR(node.dval, -1.5, 1E-6);
+}
+
+TEST(ForwardFunctions, Exponent) {
+  // Init
+  Node node;
+  Node child1(3.0, 2.0);
+  node._children.push_back(&child1);
+
+  // Exponent
+  exp_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 20.09, 1E-2);
+  EXPECT_NEAR(node.dval, 40.17, 1E-2);
+}
+
+TEST(ForwardFunctions, Logarithm) {
+  // Init
+  Node node;
+  Node child1(2.0, 1.5);
+  node._children.push_back(&child1);
+
+  // Logarithm
+  log_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 0.6931, 1E-4);
+  EXPECT_NEAR(node.dval, 0.75, 1E-4);
+}
+
+//--------------------------- Trigonometric Functions -------------------------
+TEST(ForwardFunctions, Sine) {
+  // Init
+  Node node;
+  Node child1(3.0, 2.0);
+  node._children.push_back(&child1);
+
+  // Sine
+  sin_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, 0.1411, 1E-4);
+  EXPECT_NEAR(node.dval, -1.98, 1E-2);
+}
+
+TEST(ForwardFunctions, Cosine) {
+  // Init
+  Node node;
+  Node child1(3.0, 2.0);
+  node._children.push_back(&child1);
+
+  // Cosine
+  cos_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, -0.99, 1E-4);
+  EXPECT_NEAR(node.dval, -0.28, 1E-2);
+}
+
+TEST(ForwardFunctions, Tangent) {
+  // Init
+  Node node;
+  Node child1(3.0, 2.0);
+  node._children.push_back(&child1);
+
+  // Tangent
+  tan_forward(node);
+
+  // Validate
+  EXPECT_NEAR(node.val, -0.1425, 1E-4);
+  EXPECT_NEAR(node.dval, 2.04, 1E-2);
 }
