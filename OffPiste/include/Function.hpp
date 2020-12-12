9732e9df53079b404c944710f60f0de22536ae02
aleksaleksiev <35786702+aleksaleksiev@users.noreply.github.com>
2020-12-12 15:35:51 +1100

    Update documentation for core classes

--- OffPiste/include/Function.hpp
+++ OffPiste/include/Function.hpp
@@ -15,6 +15,9 @@ typedef std::vector<std::reference_wrapper<Node>> Output;
 typedef std::vector<float> Vec;
 typedef std::vector<std::vector<float>> Mat;
 
+/**
+* A Function is composed of Nodes
+*/
 class Function {
  public:
   // constructor
@@ -24,15 +27,46 @@ class Function {
 
   friend Node;
 
-  void set_seed(Vec seeds);
+  /**
+  * Set the derivative for all input Nodes of this function
+  * @param seeds A vector containing the derivatives to apply. Values are applied in order to the input nodes.
+  * @throws std::runtime_error if the length of the provided vector is not the same as the number of input nodes.
+  */
+  void set_seed(Vec seeds); 
+  /**
+  * See OP::Function::set_seed(Vec)
+  * @see OP::Function::set_seed(Vec)
+  */
   void set_seed(Vec &&seeds);
 
-  Vec evaluate();  // evaluate each scalar function within Function
+  /**
+  * Evaluate the value of each output node in this Function by computing a forward mode pass
+  */
+  Vec evaluate();  
 
-  Mat forward_jacobian();  // using forward mode to compute jacobian matrix
+  /**
+  * Compute Jacobian matrix using forward mode
+  * @returns Mat The Jacobian matrix for this function.
+  */
+  Mat forward_jacobian();
+
+  /**
+  * Compute the derivative of this Function w.r.t the provided Node
+  * Uses forward mode
+  * @param &output_node The output node (i.e. function endpoint) to compute the derivative of
+  * @param &wrt The input node (i.e. Variable) to compute the derivative w.r.t 
+  * @returns The derivative
+  */
   float forward_derivative(Node &output_node, Node &wrt);
 
-  void zero_grad();         // set grads of all nodes in the graph f to zero
+  /**
+  * Set gradient of all Nodes in this Function to 0
+  */
+  void zero_grad();
+  /**
+  * Compute Jacobian matrix using reverse mode
+  * @returns Mat The Jacobian matrix for this function.
+  */
   Mat backward_jacobian();  // using reverse mode to compute jabobian matrix
 
  private:
--- OffPiste/include/Variable.hpp
+++ OffPiste/include/Variable.hpp
@@ -6,13 +6,25 @@
 #include "Node.hpp"
 
 namespace OP {
+
+/**
+* A Variable is a Node that represents an input to the expression. 
+* (Rather than an intermediate output).
+*/
 class Variable : public Node {
  public:
+ 	/**
+ 	* Construct a Variable with the provided initial values.
+ 	* @param val The initial value of this Variable
+ 	* @param dval The initial (i.e. seed) derivative of this Variable
+ 	* @param grad The initial (i.e. seed) gradient of this Variable
+ 	*/
   Variable(float val = 0.f, float dval = 0.f, float grad = 0.f)
       : Node(val, dval, grad) {
     _forward_func_ptr = identity_forward;
     _backward_func_ptr = identity_backward;
   }
 };
+
 }  // namespace OP
 #endif  // CS107_FINALPROJECT_VARIABLE_HPP
